cd75ec55d2679fde3e6cbecd864bba77ac329680
Diogo Nunes <dialex.nunes@gmail.com>
2017-12-12 09:16:39 +0000

    ruby code to extract price from JSON

--- ruby/helpers.rb
+++ ruby/helpers.rb
@@ -2,10 +2,14 @@ require 'rest-client'
 require 'json'
 require_relative 'logging.rb'
 
-def get_price (coin_ticker)
+def get_data (coin_ticker)
   log "Fetching data for #{coin_ticker}"
 
   url = "https://api.coinmarketcap.com/v1/ticker/#{coin_ticker}/?convert=EUR"
   response = RestClient.get(url)
   JSON.parse(response)
+end
+
+def extract_price (json)
+  json[0]['price_eur'].to_s
 end
