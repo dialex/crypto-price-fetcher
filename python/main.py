a02ee3ef01081d8a2bbbaf8943c17992ec131647
Diogo Nunes <dialex.nunes@gmail.com>
2020-06-04 14:53:47 +0100

    chore: list MVP

--- python/main.py
+++ python/main.py
@@ -5,5 +5,11 @@ print("Hello World")
 # Read config
 config_path = "config.txt"
 print("Opening file at " + config_path)
-lines = config.read_file(config_path)
-print("Read " + str(len(lines)) + " lines")
+coin_tickers = config.read_file(config_path)
+print("Read " + str(len(coin_tickers)) + " lines")
+
+# For each coin ticker
+# Call API to fetch price
+# Store that price on array
+# Write all prices to prices.txt
+# Print with colour: info, warning, err
