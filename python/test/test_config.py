ee21f504351a58a3365fc704c81483e58a0ee325
Diogo Nunes <dialex.nunes@gmail.com>
2020-06-04 14:57:00 +0100

    refactor: rename for coherence

--- python/test/test_config.py
+++ python/test/test_config.py
@@ -2,16 +2,16 @@ import unittest
 from helpers.config import read_file
 
 
-class TestHelpers(unittest.TestCase):
+class TestHelperConfig(unittest.TestCase):
 
     def test_read_file_should_output_lines(self):
-        config_path = "config.txt"
-        output = read_file(config_path)
+        file_path = "config.txt"
+        output = read_file(file_path)
         self.assertGreater(len(output), 0)
 
     def test_read_file_should_print_warning_and_return_empty_if_not_exists(self):
-        config_path = "some-file-that-doesnt-exist.txt"
-        output = read_file(config_path)
+        file_path = "some-file-that-doesnt-exist.txt"
+        output = read_file(file_path)
         self.assertEqual(len(output), 0)
 
 
