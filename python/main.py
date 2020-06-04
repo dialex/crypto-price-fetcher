41b8132f6ea3b8ccd182d8d060ec5b5de9fd2c91
Diogo Nunes <dialex.nunes@gmail.com>
2020-06-04 14:48:38 +0100

    feat: read target coins from file

--- python/main.py
+++ python/main.py
@@ -1,4 +1,9 @@
 import helpers.config as config
 
 print("Hello World")
-lines = config.read_file("config.txt")
+
+# Read config
+config_path = "config.txt"
+print("Opening file at " + config_path)
+lines = config.read_file(config_path)
+print("Read " + str(len(lines)) + " lines")
