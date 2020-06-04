e3af77f94caae9e54f7e15bd01d3c35fe013a619
Diogo Nunes <dialex.nunes@gmail.com>
2020-06-04 14:31:36 +0100

    test: alternative way to import function

--- python/test/test_config.py
+++ python/test/test_config.py
@@ -1,12 +1,12 @@
 import unittest
-import helpers.config as config
+from helpers.config import read_file
 
 
 class TestHelpers(unittest.TestCase):
 
     def test_read_file_should_output_lines(self):
-        config_filepath = "config.txt"
-        output = config.read_file(config_filepath)
+        config_path = "config.txt"
+        output = read_file(config_path)
         self.assertGreater(len(output), 0)
 
 
