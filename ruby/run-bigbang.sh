0d13ea38871ab97585a0f4531eda16e9c3dec4c1
Diogo Nunes <dialex.nunes@gmail.com>
2021-07-31 20:04:29 +0100

    update versions and use rbenv instead of rvm

--- ruby/run-bigbang.sh
+++ ruby/run-bigbang.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo -e '\n>>> Installing RVM...'
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-curl -sSL https://get.rvm.io | bash
+echo -e '\n>>> Installing Ruby version manager...'
+brew install rbenv
 
 echo -e '\n>>> Installing Ruby...'
-rvm install ruby
-rvm --default use ruby
+rbenv install 2.6.8
+rbenv local 2.6.8
 
 echo -e '\n>>> Installing Ruby tools...'
-gem install bundler --no-rdoc --no-ri
-gem install rake --no-rdoc --no-ri
+gem install bundler
+gem install rake
+rake install
