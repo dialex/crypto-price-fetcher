442b47a7e941bd985d8553149920937bbefae3ab
Diogo Nunes <dialex.nunes@gmail.com>
2022-01-02 15:26:17 +0000

    fix: make bigbang script more robust

--- python/run-bigbang.sh
+++ python/run-bigbang.sh
@@ -3,6 +3,7 @@ version='3.9.6'
 
 echo -e '\n>>> Installing Python version manager...'
 brew install pyenv
+brew install pyenv-virtualenv
 
 echo -e '\n>>> Installing Python...'
 pyenv install $version
@@ -11,7 +12,7 @@ python3 -V
 pip3 -V
 
 echo -e '\n>>> Installing app dependencies...'
-pip3 install pipenv
+sudo -H pip3 install -U pipenv
 pipenv --python $version
 pipenv install
 pipenv run pip list
