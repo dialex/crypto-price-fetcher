1b0ca88d6163d03334b1d20b9440c03bdf1db897
Diogo Nunes <dialex.nunes@gmail.com>
2025-07-13 15:02:13 +0100

    chore: update python version

--- python/run-bigbang.sh
+++ python/run-bigbang.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-version='3.10.2'
+version='3.13.5'
 
 echo -e '\n>>> Installing Python version manager...'
 brew install pyenv
@@ -8,12 +8,12 @@ brew install pyenv-virtualenv
 echo -e '\n>>> Installing Python...'
 pyenv install $version
 pyenv local $version
-python3 -V
-pip3 -V
+python -V
+pip -V
 
 echo -e '\n>>> Installing app dependencies...'
-python3 -m pip install --upgrade pip
-python3 -m pip install pipenv
+python -m pip install --upgrade pip
+python -m pip install pipenv
 pipenv --python $version
 pipenv install
 pipenv run pip list
